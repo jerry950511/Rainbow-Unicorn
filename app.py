4c1594505a7eff2df67b3f6b425a245242fed55b
csiejar <j0970238552@gmail.com>
2023-11-16 16:11:27 +0800

    123

--- app.py
+++ app.py
@@ -23,8 +23,9 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
+    message = event.message.text
+    if message == "晚餐吃什麼":
+        line_bot_api.reply_message(event.reply_token, "去吃屎啦")
 
 import os
 if __name__ == "__main__":
