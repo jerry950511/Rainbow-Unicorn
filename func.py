b748b8b47d23a7504a47a4257ecf31429f82e87d
csiejar <j0970238552@gmail.com>
2023-11-18 19:01:37 +0800

    asda

--- func.py
+++ func.py
@@ -3,7 +3,10 @@ import random
 def get_one_restaurant():
     with open('restaurants.json', 'r', encoding='utf8') as f:
         restaurant = json.load(f)
-    return random.choice(restaurant)
+    if len(restaurant) == 0:
+        return
+    else:
+        return random.choice(restaurant)
 
 def get_all_restaurant():
     with open('restaurants.json', 'r', encoding='utf8') as f:
