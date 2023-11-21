0274c230646a79e6771ca49bf0e2377ae22956ea
csiejar <j0970238552@gmail.com>
2023-11-21 14:06:24 +0800

    adsfadsf

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
 import os
-from func import *
+from sheet import *
 
 app = Flask(__name__)
 
@@ -42,27 +42,15 @@ def handle_message(event):
         else:
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text=",".join(all_restaurant)))
     elif message.startswith("新增餐廳:") or message.startswith("新增餐廳："):
-        with open('restaurants.json', 'r', encoding='utf8') as f:
-            restaurant = json.load(f)
-        if message[5:] in restaurant:
+        if add_restaurant(message[5:]):
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="已將"+message[5:]+"新增至餐廳選擇器中"))
+        else:
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text="餐廳已存在"))
-            return
-        else:
-            restaurant.append(message[5:])
-            with open('restaurants.json', 'w', encoding='utf8') as f:
-                json.dump(restaurant, f, ensure_ascii=False)
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="已新增 - "+message[5:]+"到餐廳選擇器中"))
     elif message.startswith("刪除餐廳:") or message.startswith("刪除餐廳："):
-        with open('restaurants.json', 'r', encoding='utf8') as f:
-            restaurant = json.load(f)
-        if message[5:] not in restaurant:
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="餐廳不存在"))
-            return
-        else:
-            restaurant.remove(message[5:])
+        if delete_restaurant(message[5:]):
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text="已將"+message[5:]+"從餐廳選擇器中刪除"))
-            with open('restaurants.json', 'w', encoding='utf8') as f:
-                json.dump(restaurant, f, ensure_ascii=False)
+        else:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="餐廳不存在"))
 
     elif message == "指令" or message == "提示":
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text="指令列表:\n指令名稱: 晚餐吃什麼\n指令說明:從晚餐選擇器中隨機抽出一間來吃\n\n指令名稱: 現在餐廳\n指令說明:顯示在晚餐選擇器中的餐廳\n\n指令名稱: 備份現在餐廳\n指令說明:輸出現在所有餐廳並用逗號分隔(嘎睿備份用)\n\n指令名稱: 新增餐廳:<餐廳名>\n指令名稱:新增餐廳至餐廳選擇器中\n\n指令名稱: 刪除餐廳:<餐廳名>\n指令名稱:從餐廳選擇器中刪除餐廳"))
---
+++ sheet.py
@@ -0,0 +1,37 @@
+import pygsheets
+import random
+gc = pygsheets.authorize(service_file='dinnerchooser-405509-33d9f0bb2be8.json')
+sht = gc.open_by_url('https://docs.google.com/spreadsheets/d/1uf7UIUgLfVhhGmEMeGEqa5tdYFZ6gHzeW9Jm3Dzpy-I/edit?usp=sharing')
+
+def get_one_restaurant():
+    wks = sht.worksheet_by_title('餐廳選擇器')
+    val = wks.get_col(1, include_tailing_empty=False)[1:]
+    if len(val) == 0:
+        return
+    else:
+        val = random.choice(val)
+        return val
+
+def get_all_restaurant():
+    wks = sht.worksheet_by_title('餐廳選擇器')
+    val = wks.get_col(1, include_tailing_empty=False)[1:]
+    quantity = len(val)
+    return val, quantity
+    
+def add_restaurant(restaurant):
+    wks = sht.worksheet_by_title('餐廳選擇器')
+    val = wks.get_col(1, include_tailing_empty=False)[1:]
+    if restaurant in val:
+        return False
+    else:
+        wks.insert_rows(1, values=restaurant)
+        return True
+    
+def delete_restaurant(restaurant):
+    wks = sht.worksheet_by_title('餐廳選擇器')
+    val = wks.get_col(1, include_tailing_empty=False)[1:]
+    if restaurant in val:
+        wks.delete_rows(val.index(restaurant)+2)
+        return True
+    else:
+        return False
